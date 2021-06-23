3f52a15e8ddbe9eef04feef36010fec66d8ebb95
Julius Foitzik <info@accountr.eu>
2021-06-23 20:31:09 +0200

    Make faster/better vesion without nested for-loop

--- pkg/merge/merge.go
+++ pkg/merge/merge.go
@@ -8,36 +8,35 @@ func Merge(intervals [][]int) [][]int {
 	if len(intervals) < 2 {
 		return intervals
 	}
+	// Ordering the list of intervals by the first interval value
+	// helps us make assumptions that are later needed for the algorithm to work.
+	//
+	// Why?
+	// Ordering by the first item means that for each item the right / next neighbor interval
+	// is guaranteed to either start at the same low end or is greater than it.
+	// This let's us assume that each right neighbor is either "mergeable" or not,
+	// implying that the n-th neighbor is not "mergeable" if the next neighbor is not.
+	// Therefore no look-ahead other than a 1-step look-ahead is needed, which enables
+	// us to traverse the sorted list linearly.
 	sort.Slice(intervals, func(i, j int) bool {
 		return intervals[i][0] <= intervals[j][0]
 	})
 	res := make([][]int, 0)
-	for i := range intervals {
-		var current []int
-		if len(res) > 0 {
-			current = res[len(res)-1] // last added interval
+	// This is bootstrap code
+	// In order to make the for-loop run smooth
+	// we need to add the first item to the results otherwise the whole list is off
+	// by one, i.e., the first interval is missing.
+	currentSoFar := intervals[0]
+	res = append(res, currentSoFar)
+	for i := range intervals { // iterate linearly
+		// over the list of intervals
+		if intersects(currentSoFar, intervals[i]) { // if it intersects we create a
+			// "merged" interval containing both (the one so far and the current interval we are looking at)
+			currentSoFar = mergedInterval(currentSoFar, intervals[i])
+		} else { // if not no merge can be performed, just append it and mark it as current
+			currentSoFar = intervals[i]
+			res = append(res, currentSoFar)
 		}
-
-		if current != nil {
-			// check if the next entry (i) can be merged with current
-			if intersects(intervals[i], current) {
-				// if so, update current
-				res[len(res)-1] = mergedInterval(intervals[i], current)
-				continue
-			}
-		}
-
-		// the current interval does not intersect with the next entry (i), let's find overlaps in the upcoming
-		// entries (j)
-
-		// compare i to every other entry (j) and select largest interval
-		currentSoFar := intervals[i]
-		for j := range intervals[i:] {
-			if intersects(currentSoFar, intervals[j]) {
-				currentSoFar = mergedInterval(currentSoFar, intervals[j])
-			}
-		}
-		res = append(res, currentSoFar)
 	}
 	return res
 }
